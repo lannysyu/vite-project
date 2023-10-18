e30d1cabcaafae88a6721c48291aa8bbdb20fef0
KJSOFT\lanny.syu <oopsjohn.77@gmail.com>
2023-10-18 15:31:41 +0800

    new file

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Refence
+
+builder.io
+https://www.builder.io/c/docs/quickstart#developer-quickstart
+
+youtube
+https://www.youtube.com/watch?v=e44LurA0Qb4
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,30 +7,6 @@ defineProps({
 
 const count = ref(0);
 
-import { onMounted } from "vue";
-// OR use these if you are on Vue 3
-import {
-  fetchOneEntry,
-  RenderContent,
-  isPreviewing,
-} from "@builder.io/sdk-vue/vue3";
-import "@builder.io/sdk-vue/vue3/css";
-
-const canShowContent = ref(false);
-const content = ref(null);
-
-onMounted(() => {
-  fetchOneEntry({
-    model: "page",
-    apiKey: "057741175755416385a1000c5f69003a", // Replace with your own API key
-    userAttributes: {
-      urlPath: window.location.pathname,
-    },
-  }).then((res) => {
-    content.value = res;
-    canShowContent.value = content.value || isPreviewing();
-  });
-});
 </script>
 
 <template>
@@ -48,9 +24,8 @@ onMounted(() => {
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
@@ -58,15 +33,6 @@ onMounted(() => {
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-
-  <div>
-      <RenderContent
-        v-if="canShowContent"
-        model="page"
-        :content="content"
-        :api-key="apiKey"
-      />
-    </div>
 </template>
 
 <style scoped>
